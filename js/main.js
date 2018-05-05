7008a4bde795ea4307133c070e4d5e5fe9e8c94d
Holly <ha.bee4@gmail.com>
2018-05-05 14:49:18 -0500

    javascript part I completed

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("This file is linked!")
+
+var teacherName = 'Tye Leong';
+// var teacherName = ['Tye Leong','Alex Winter','Sally Jones','Tracy Flowers','Phil Cube'];
+var department = 'Photography';
+// var department = ['Photography','Medicine','Art & Design','Nutrition','Physics'];
+var rating1 = 3.5;
+var rating2 = 4.4;
+var rating3 = 2.8;
+
+var avgRating = (rating1 + rating2 + rating3) / 3;
+
+console.log("Teacher: " + teacherName);
+console.log("Department: " + department);
+console.log("Ratings: " + rating1 + "," + rating2 + "," + rating3);
+console.log("Average Rating: " + avgRating);
