e3f1480ecc6e6050565e6a19a025a9690ade3928
Holly <ha.bee4@gmail.com>
2018-05-07 22:15:04 -0500

    final steps of challenge 1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
 console.log("This file is linked!")
 
-var teacherName = 'Tye Leong';
+// Index Variables
+var teacherName
+teacherName = 'Tye Leong';
 // var teacherName = ['Tye Leong','Alex Winter','Sally Jones','Tracy Flowers','Phil Cube'];
-var department = 'Photography';
+var department
+department = 'Photography';
 // var department = ['Photography','Medicine','Art & Design','Nutrition','Physics'];
 var rating1 = 3.5;
 var rating2 = 4.4;
 var rating3 = 2.8;
 
 var avgRating = (rating1 + rating2 + rating3) / 3;
+var rounded = Math.round( avgRating * 10 ) / 10;
 
 console.log("Teacher: " + teacherName);
 console.log("Department: " + department);
 console.log("Ratings: " + rating1 + "," + rating2 + "," + rating3);
-console.log("Average Rating: " + avgRating);
+console.log("Average Rating: " + rounded);
+
+// STUDENT Variables
+
+var studentName = "Oprah Winfrey"
+var studentMajor = "Empathy"
+var studentEmail = "Oprah@gmail.com"
+var studentGPA = 6.0
+
+console.log("Student: " + studentName);
+console.log("Major: " + studentMajor);
+console.log("Email: " + studentEmail);
+console.log("GPA: " + studentGPA);
+
+
+// Courses Variables
+var courseName = "Health"
+var courseTeacher = "Tracy Flowers"
+var semester = "Fall 2017"
+
+console.log("Course: " + courseName);
+console.log("Teacher: " + courseTeacher);
+console.log("Semester: " + semester);
